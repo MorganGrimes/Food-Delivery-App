0413e3cd148eb9ad3c7741262c867cc150bcd2e5
Salvio96 <venerusofamily@hotmail.it>
2025-05-16 16:18:39 +0200

    adding navigation from splash to login

--- app/src/main/java/com/example/fooddeliveryapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/fooddeliveryapp/ui/splash/SplashFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.fooddeliveryapp.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
@@ -14,5 +18,15 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val navController = findNavController()
+            delay(2000)
+            navController.navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
+        }
     }
 }
