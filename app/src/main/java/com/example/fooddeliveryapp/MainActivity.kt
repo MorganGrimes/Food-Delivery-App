6c7af3821f0d990fb95fc2a44d1e066d70a853eb
Salvio96 <venerusofamily@hotmail.it>
2025-05-16 16:18:15 +0200

    adding navigation host

--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
@@ -3,10 +3,24 @@ package com.example.fooddeliveryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fooddeliveryapp.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavHostController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController as NavHostController
+        val view = binding.root
         enableEdgeToEdge()
+        setContentView(view)
     }
 }
