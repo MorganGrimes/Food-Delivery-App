3557e4cbd1fb4422143ac690a889fa5fbfe183e6
Salvio96 <venerusofamily@hotmail.it>
2025-05-19 12:58:06 +0200

    removing warnings to all fragments, adding recycler to home fragment,

--- app/src/main/java/com/example/fooddeliveryapp/ui/locationaccess/LocationAccessFragment.kt
+++ app/src/main/java/com/example/fooddeliveryapp/ui/locationaccess/LocationAccessFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.fooddeliveryapp.R
 
-class LocationAcessFragment : Fragment() {
+class LocationAccessFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
