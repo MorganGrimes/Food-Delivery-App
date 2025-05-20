77cf8103946b5d08d9f6a8ad5ad33c0c6e4861f8
Salvio96 <venerusofamily@hotmail.it>
2025-05-20 18:49:54 +0200

    adding coupon dialog, food details fragments and recycler popular food

---
+++ app/src/main/java/com/example/fooddeliveryapp/ui/coupon/CouponDialogFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fooddeliveryapp.ui.coupon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fooddeliveryapp.R
+
+class CouponDialogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_coupon_dialog, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/fooddeliveryapp/ui/fooddetails/FoodDetailsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.fooddeliveryapp.ui.fooddetails
+
+class FoodDetailsViewModel {
+}
