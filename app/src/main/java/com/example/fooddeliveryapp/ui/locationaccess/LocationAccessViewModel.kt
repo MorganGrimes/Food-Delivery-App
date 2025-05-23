c58a6aedb0306aff236216be6431d236e4bf3678
Salvio96 <venerusofamily@hotmail.it>
2025-05-23 12:59:42 +0200

    renaming most of textview

--- app/src/main/java/com/example/fooddeliveryapp/ui/locationaccess/LocationAccessViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.fooddeliveryapp.ui.locationaccess
-
-class LocationAccessViewModel {
-}
--- app/src/main/java/com/example/fooddeliveryapp/ui/search/SearchViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.fooddeliveryapp.ui.search
-
-class SearchViewModel {
-}
