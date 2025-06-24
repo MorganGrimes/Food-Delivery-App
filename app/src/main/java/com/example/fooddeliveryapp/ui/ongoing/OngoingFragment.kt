2c4707740cbbc2f213be9aac5df1b9110322c694
Salvio96 <venerusofamily@hotmail.it>
2025-06-24 14:13:46 +0200

    removed imports

--- app/src/main/java/com/example/fooddeliveryapp/ui/ongoing/OngoingFragment.kt
+++ app/src/main/java/com/example/fooddeliveryapp/ui/ongoing/OngoingFragment.kt
@@ -5,17 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.fooddeliveryapp.R
 import com.example.fooddeliveryapp.data.local.database.AppDatabase
-import com.example.fooddeliveryapp.data.model.OrderItemModel
 import com.example.fooddeliveryapp.data.repository.OrderRepository
 import com.example.fooddeliveryapp.databinding.FragmentOngoingBinding
 import com.example.fooddeliveryapp.ui.adapters.OrderRecyclerAdapter
-import com.example.fooddeliveryapp.ui.home.HomeViewModel
 
 class OngoingFragment : Fragment() {
 
